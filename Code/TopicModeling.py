605a6a2ec448ce2edbd22b5dc37b205d69eeaee8
Nawel Medjkoune <nawel@users.noreply.github.com>
2016-11-11 15:15:11 +0100

    fixing

--- Code/TopicModeling.py
+++ Code/TopicModeling.py
@@ -75,7 +75,7 @@ for dir_annee in list_dir:
         sub_sub_dirs = os.listdir(chemin_corpus+dir_annee+"/"+sub_dir)
         for sub_sub_dir in sub_sub_dirs:
             files = os.listdir(chemin_corpus+dir_annee+"/"+sub_dir+"/"+sub_sub_dir)
-            for f in files[0:2]:
+            for f in files:
                 open_file = codecs.open(chemin_corpus+dir_annee+"/"+sub_dir+"/"+sub_sub_dir+"/"+f, "r", encoding="utf-8")
                 content_file = open_file.read()
                 if detect(content_file)== "fr": #Detecter la langue
@@ -137,13 +137,13 @@ def doc_lengths(texts):
 
 
 
-def doc_topic_dists(corpus, lda):
+def doc_topic_dists(corpus, lda, num_topics):
     doc_topic_dists = []
     for doc in range(len(corpus)):
-        list_topic_proba = []
+        list_topic_proba = [0]*num_topics
         temp = lda.get_document_topics(corpus[doc], minimum_probability=0)
         for topic, proba in temp:
-            list_topic_proba.append(proba)
+            list_topic_proba[topic]=proba
         doc_topic_dists.append(list_topic_proba)
     return doc_topic_dists
 doc_topic_dists = doc_topic_dists(corpus, lda)
